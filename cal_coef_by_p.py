b4f4e918932016c33344a10db4db27cf221f6332
Jaeung Hyun <jaeung@me.com>
2021-05-10 14:28:09 +0900

    change allele list

--- cal_coef_by_p.py
+++ cal_coef_by_p.py
@@ -31,15 +31,15 @@ with open('/home/jaeung/Research/MHC/ms+ba_short_hla_gradcam_result_remove_nan.p
 allele_list = list(p9_binder.keys())
 
 
-group1 = ['HLA-A-2403', 'HLA-A-2402','HLA-A-2413','HLA-A-2301','HLA-A-2406','HLA-A-2407',]
-group2 = ['HLA-A-3303', 'HLA-A-3301','HLA-A-6801','HLA-A-6601','HLA-A-3401','HLA-A-6602',
-          'HLA-A-3101','HLA-A-7401',]
-group3 = ['HLA-A-3001', 'HLA-A-0301', 'HLA-A-1101', 'HLA-A-1102','HLA-A-6812']
-group4 = ['HLA-A-6802','HLA-A-6901']
-group5 = ['HLA-A-0205','HLA-A-0206','HLA-A-0217','HLA-A-0216','HLA-A-0212','HLA-A-0219',
-          'HLA-A-0207','HLA-A-0203','HLA-A-0201','HLA-A-0211','HLA-A-0204','HLA-A-0202',]
-group6 = ['HLA-A-2601','HLA-A-2501','HLA-A-2608','HLA-A-2603','HLA-A-2602',]
-group7 = ['HLA-A-0103','HLA-A-0101','HLA-A-2902','HLA-A-3002','HLA-A-3601','HLA-A-8001',]
+group1 = ['HLA-A-2403', 'HLA-A-2402', 'HLA-A-2413', 'HLA-A-2301', 'HLA-A-2406', 'HLA-A-2407']
+group2 = ['HLA-A-3303', 'HLA-A-3301', 'HLA-A-6801', 'HLA-A-6601', 'HLA-A-3401', 'HLA-A-6602',
+          'HLA-A-3101', 'HLA-A-7401']
+group3 = ['HLA-A-3001', 'HLA-A-0301', 'HLA-A-1101', 'HLA-A-1102', 'HLA-A-6812']
+group4 = ['HLA-A-6802', 'HLA-A-6901']
+group5 = ['HLA-A-0205', 'HLA-A-0206', 'HLA-A-0217', 'HLA-A-0216', 'HLA-A-0212', 'HLA-A-0219',
+          'HLA-A-0207', 'HLA-A-0203', 'HLA-A-0201', 'HLA-A-0211', 'HLA-A-0204', 'HLA-A-0202']
+group6 = ['HLA-A-2601', 'HLA-A-2501', 'HLA-A-2608', 'HLA-A-2603', 'HLA-A-2602']
+group7 = ['HLA-A-0103', 'HLA-A-0101', 'HLA-A-2902', 'HLA-A-3002', 'HLA-A-3601', 'HLA-A-8001']
 
 
 for g in tqdm(['group1', 'group2', 'group3', 'group4', 'group5', 'group6', 'group7']):
