0db76ff6466bc0da51fbee54cae1e46965b2066d
Jaeung Hyun <jaeung@me.com>
2021-09-21 16:01:20 +0900

    change file_mod.py

--- file_mod.py
+++ file_mod.py
@@ -11,6 +11,7 @@ group_mode = sys.argv[3]  # ingroup or outgroup
 for p in range(9):
     print('importing binder data')
     data_list = load_target_gradcam_result(allele, mode, 0, p, cp=None)  # 어짜피 polar안에 다 있음 다른 cp들 결과
+
     if mode == 'hydro':
         v = 0
     elif mode == 'bulky':
@@ -24,30 +25,30 @@ for p in range(9):
 
     key_list = []
     for data in data_list:
-        key_list.extend(list(data.keys()))
-        #for list_ in data:
-            #key_list.extend(list(list_.keys()))
+        #key_list.extend(list(data.keys()))
+        for list_ in data:
+            key_list.extend(list(list_.keys()))
 
     cp_result = {}
-    #result = {}
+    result = {}
     for key in key_list:
         cp_result[key] = []
-        #result[key] = []
+        result[key] = []
 
     print('cp result')
     for data in data_list:
         #key_lists = []
         #for key in data[0].keys():
         for key in data.keys():
-            for i, value in enumerate(data[key]):
-            #for i, value in enumerate(data[0][key]):
+            #for i, value in enumerate(data[key]):
+            for i, value in enumerate(data[0][key]):
                 try:
                     cp_result[key].append(value)
                 except:
                     print(value)
-        #for key in data[1].keys():
-        #    for value in data[1][key]:
-        #        result[key].append(value.astype('float16'))
+        for key in data[1].keys():
+            for value in data[1][key]:
+                result[key].append(value)
     with open(f'/home/jaeung/Research/MHC/short_{allele}_{mode}_{group_mode}_{p+1}_with_gradcam_result.pkl', 'wb') as f:
-        #pickle.dump((cp_result, result), f)
-        pickle.dump((cp_result), f)
+        pickle.dump((cp_result, result), f)
+        #pickle.dump((cp_result), f)
--- find_property_combination.py
+++ find_property_combination.py
@@ -19,25 +19,15 @@ def check_combi(pep, mode):
 def find_property(df, target_group, binder, allele, target, mode, p):
     result = {}
     result[allele] = []
-    df = df[df['allele'].isin(target_group)] # HLA-A,B,C 각각 가져오는 부분
+    df = df[df['allele'].isin(target_group)]  # HLA-A,B,C 각각 가져오는 부분
     for num, pepseq in enumerate(df.loc[df['allele'] == allele]['Peptide seq']):
         if check_combi(pepseq[p], mode) == target:
             result[allele].append(binder[allele][num][:, p])
-            #new_array[:, w] = binder[allele][num][:, w]
+            # new_array[:, w] = binder[allele][num][:, w]
 
-        #result[allele].append(new_array)
+        # result[allele].append(new_array)
 
     return result
-
-
-def check_combi(pep, mode):
-    target1 = aa_property[aa_property[mode] == 1]['aa'].tolist()
-    target2 = aa_property[aa_property[mode] == 0]['aa'].tolist()
-
-    if pep in target1:
-        return 0
-    elif pep in target2:
-        return 1
 
 
 def find_property_value(aa, mode):
@@ -51,9 +41,9 @@ def find_property_value(aa, mode):
         v = 5
     elif mode == 'MW':
         v = 6
-    #print(v)
+    # print(v)
     value = aa_property.loc[aa_property['aa'] == aa.upper()].values[0][v]
-    #print(value)
+    # print(value)
     try:
         return value
     except:
@@ -68,11 +58,14 @@ def find_property2(df, target_group, binder, allele, mode, p):
     cor_result[allele] = []
     cp_value[allele] = []
     df = df[df['allele'].isin(target_group)]  # HLA-A,B,C 각각 가져오는 부분
-    for num, pepseq in enumerate(df.loc[df['allele'] == allele]['Peptide seq']):
+    for num, pepseq in enumerate(df.loc[df['allele'] == allele]['Peptide seq'].to_numpy()):
+        # print(p)
+        # print(num)
         cor_result[allele].append(binder[allele][num][:, p])
         cp_value[allele].append(find_property_value(pepseq[p], mode))
 
     return cp_value, cor_result
+
 
 @ray.remote
 def find_property3(df, target_group, binder, allele, mode, p):
@@ -88,16 +81,13 @@ def find_property3(df, target_group, binder, allele, mode, p):
 print(sys.argv[1])
 print(sys.argv[2])
 
-#object_store_memory = int(0.6 * ray.utils.get_system_memory() // 10 ** 9 * 10 ** 9)
 try:
     ray.init(address='auto')
 except:
-    ray.init(_plasma_directory="/home/jaeung/tmp", object_store_memory=10 ** 9 * 10 ** 9)
-    # ray.init(dashboard_host='0.0.0.0',
-    #          log_to_driver=False,
-    #          _plasma_directory="/home/jaeung/tmp",
-    #          object_store_memory=object_store_memory
-    #          )
+    ray.init(dashboard_host='0.0.0.0',
+             log_to_driver=False,
+             _plasma_directory="/home/jaeung/tmp"
+             )
 
 p9_binder = load_gradcam_result()
 p9_binder_id = ray.put(p9_binder)
@@ -126,24 +116,27 @@ for allele, mode in list(product(*item)):
         total_g.extend(g)
     for p in range(9):
         if sys.argv[3] == '0':
-            result = ray.get([find_property.remote(df_id, total_g, p9_binder_id, allele, target, mode, p)
+            result = ray.get([find_property.remote(df_id, total_g, p9_binder_id, allele, mode, p)
                               for allele in total_g])
             print('Saving Result')
-            with open(f'/home/hdd/{allele}_{mode}_position_{p+1}_gradcam_result.pkl',
+            with open(f'/data/result/{allele}_position_{p + 1}_gradcam_result.pkl',
                       'wb') as f:
                 pickle.dump(result, f)
+
         elif sys.argv[3] == 'next':
             result = ray.get([find_property3.remote(df_id, total_g, p9_binder_id, allele, mode, p)
                               for allele in total_g])
             print('Saving Result')
             with open(
-                    f'/home/hdd/{allele}_{mode}_position_{p + 1}_gradcam_result_with_cp_value.pkl',
+                    f'/data/result/{allele}_{mode}_position_{p + 1}_gradcam_result_with_cp_value.pkl',
                     'wb') as f:
                 pickle.dump(result, f)
+
         else:
             result = ray.get([find_property2.remote(df_id, total_g, p9_binder_id, allele, mode, p)
                               for allele in total_g])
             print('Saving Result')
-            with open(f'/home/hdd/{allele}_{mode}_position_{p+1}_gradcam_result_with_cp_value.pkl',
+            with open(f'/data/result/{allele}_{mode}_position_{p + 1}_gradcam_result_with_cp_value.pkl',
                       'wb') as f:
                 pickle.dump(result, f)
+
