50f0393022182c1e89d384730bd1ac20c800a1b6
Jaeung Hyun <jaeung@me.com>
2021-09-21 16:24:32 +0900

    add logic in file_mod.py

--- file_mod.py
+++ file_mod.py
@@ -7,6 +7,7 @@ import pickle
 allele = sys.argv[1]
 mode = sys.argv[2]  #뭐였더라..
 
+
 for p in range(9):
     print('importing binder data')
     data_list = load_target_gradcam_result(allele, mode, 0, p, cp=None)  # 어짜피 polar안에 다 있음 다른 cp들 결과
@@ -16,30 +17,49 @@ for p in range(9):
         #key_list.extend(list(data.keys()))
         for list_ in data:
             key_list.extend(list(list_.keys()))
+    if mode == 'polar':
+        cp_result = {}
+        result = {}
+        for key in key_list:
+            cp_result[key] = []
+            result[key] = []
 
-    cp_result = {}
-    result = {}
-    for key in key_list:
-        cp_result[key] = []
-        result[key] = []
+        print('cp result')
+        for data in data_list:
+            #key_lists = []
+            for key in data[0].keys():
+            #for key in data.keys():
+                #for i, value in enumerate(data[key]):
+                for i, value in enumerate(data[0][key]):
+                    try:
+                        cp_result[key].append(value)
+                    except:
+                        print(value)
+            for key in data[1].keys():
+                for value in data[1][key]:
+                    result[key].append(value)
 
-    print('cp result')
-    for data in data_list:
-        #key_lists = []
-        for key in data[0].keys():
-        #for key in data.keys():
-            #for i, value in enumerate(data[key]):
-            for i, value in enumerate(data[0][key]):
-                try:
-                    cp_result[key].append(value)
-                except:
-                    print(value)
-        for key in data[1].keys():
-            for value in data[1][key]:
-                result[key].append(value)
+        with open(f'/data/result/short_{allele}_{p+1}_with_gradcam_by_position.pkl', 'wb') as f:
+            pickle.dump(cp_result, f)
+        with open(f'/data/result/short_{allele}_{mode}_{p+1}_with_cp_value.pkl', 'wb') as f:
+            pickle.dump(result, f)
+    else:
+        cp_result = {}
+        for key in key_list:
+            cp_result[key] = []
 
-    with open(f'/data/result/short_{allele}_{p+1}_with_gradcam_by_position.pkl', 'wb') as f:
-        pickle.dump(cp_result, f)
-    with open(f'/data/result/short_{allele}_{mode}_{p+1}_with_cp_value.pkl', 'wb') as f:
-        pickle.dump(result, f)
+        print('cp result')
+        for data in data_list:
+            # key_lists = []
+            #for key in data[0].keys():
+            for key in data.keys():
+                for i, value in enumerate(data[key]):
+                #for i, value in enumerate(data[0][key]):
+                    try:
+                        cp_result[key].append(value)
+                    except:
+                        print(value)
+        with open(f'/data/result/short_{allele}_{p + 1}_with_gradcam_by_position.pkl', 'wb') as f:
+            pickle.dump(cp_result, f)
+
 
